3593773dafc96e8340c810bd6a34c62d731a5d71
TinyChou <zhoujinfu0124@163.com>
2022-11-30 20:20:25 +0800

    feat(impl): initial universe support with random using `js-sys::Math::random(), cells using bits instead of Cells to save memory`

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ mod utils;
 use wasm_bindgen::prelude::*;
 use std::fmt;
 
+extern crate js_sys;
+extern crate fixedbitset;
+
+use fixedbitset::FixedBitSet;
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -31,7 +36,7 @@ pub enum Cell {
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: Vec<Cell>,
+    cells: FixedBitSet,
 }
 
 impl Universe {
@@ -66,15 +71,11 @@ impl Universe {
         let width = 64;
         let height = 64;
 
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+        for i in 0..size {
+            cells.set(i, i % 2 == 0 || i % 7 == 0);
+        }
         
         Self {
             width,
@@ -95,8 +96,8 @@ impl Universe {
         self.height
     }
 
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
     }
 
     pub fn tick(&mut self) {
@@ -108,24 +109,13 @@ impl Universe {
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbors
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three libe neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live call with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
+                next.set(idx, match (cell, live_neighbors) {
+                    (true, x) if x < 2 => false,
+                    (true, 2) | (true, 3) => true,
+                    (true, x) if x > 3 => false,
+                    (false, 3) => true,
                     (otherwise, _) => otherwise,
-                };
-
-                next[idx] = next_cell;
+                });
             }
         }
 
@@ -137,7 +127,7 @@ impl fmt::Display for Universe {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for line in self.cells.as_slice().chunks(self.width as usize) {
             for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                let symbol = if cell == 0 { '◻' } else { '◼' };
                 write!(f, "{}", symbol)?;
             }
             write!(f, "\n")?;
--- wasm-app/src/counter.ts
+++ wasm-app/src/counter.ts
@@ -1,4 +1,4 @@
-import { Universe, Cell } from 'wasm-game-of-life'
+import { Universe } from 'wasm-game-of-life'
 import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg.wasm'
 
 export function setupCounter(element: HTMLButtonElement) {
@@ -48,17 +48,22 @@ export function setupWasmGreet(element: HTMLButtonElement) {
       ctx.stroke()
     }
     const getIndex = (row: number, column: number) => row * width + column
+    const bitIsSet = (n, arr) => {
+      const byte = Math.floor(n / 8)
+      const mask = 1 << (n % 8)
+      return (arr[byte] & mask) === mask
+    }
     const drawCells = () => {
       if (!ctx) return
       const cellsPtr = universe.cells()
-      const cells = new Uint8Array(memory.buffer, cellsPtr, width * height)
+      const cells = new Uint8Array(memory.buffer, cellsPtr, width * height / 8)
 
       ctx.beginPath()
       for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
           const idx = getIndex(row, col)
 
-          ctx.fillStyle = cells[idx] == Cell.Dead ? DEAD_COLOR : ALIVE_COLOR
+          ctx.fillStyle = bitIsSet(idx, cells) ? DEAD_COLOR : ALIVE_COLOR
           ctx.fillRect(
             col * (CELL_SIZE + 1) + 1,
             row * (CELL_SIZE + 1) + 1,

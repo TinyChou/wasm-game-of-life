438774807cbe112b2a09965a8dae252ddba09f21
TinyChou <zhoujinfu0124@163.com>
2022-11-30 09:33:44 +0800

    feat: wasm Hello World!

---
+++ wasm-app/src/counter.ts
@@ -0,0 +1,15 @@
+import * as wasm from 'wasm-game-of-life'
+
+export function setupCounter(element: HTMLButtonElement) {
+  let counter = 0
+  const setCounter = (count: number) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
+
+export function setupWasmGreet(element: HTMLButtonElement) {
+  element.addEventListener('click', () => wasm.greet())
+}

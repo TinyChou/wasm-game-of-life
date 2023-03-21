7c362752a9ec5eda02ec6eaafec6478391d405d2
TinyChou <zhoujinfu0124@163.com>
2023-03-21 19:31:53 +0800

    feat(canvas): using canvas to render the wasm memory linear bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-use std::fmt;
+use std::{fmt, cell};
+use fixedbitset::FixedBitSet;
 
 // When the `wee_alloc` feature is enabled,
 // use `wee_alloc` as the global allocator.
@@ -9,6 +10,8 @@ use std::fmt;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+extern crate js_sys;
+extern crate fixedbitset;
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
@@ -62,6 +65,19 @@ impl Universe {
 // Public methods, exported to JavaScript
 #[wasm_bindgen]
 impl Universe {
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
     pub fn new() -> Universe {
         let width = 64;
         let height = 64;
@@ -76,6 +92,43 @@ impl Universe {
             })
             .collect();
         
+        Universe { width, height, cells, }
+    }
+
+    // Exercises 4.4 (1)
+    pub fn new_single() -> Universe {
+        let width = 64;
+        let height = 64;
+        let rand = (js_sys::Math::random() * (width as f64) * (height as f64)) as u32;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if rand == i {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+    
+        Universe { width, height, cells, }
+    }
+
+    // Exercises 4.4 (2)
+    pub fn new_random() -> Universe {
+        let width = 64;
+        let height = 54;
+
+        let cells = (0..width * height)
+            .map(|_| {
+                if js_sys::Math::random() < 0.5 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
         Universe { width, height, cells, }
     }
 
@@ -128,5 +181,88 @@ impl fmt::Display for Universe {
         }
 
         Ok(())
+    }
+}
+
+#[wasm_bindgen]
+pub struct BitUniverse {
+    width: u32,
+    height: u32,
+    cells: FixedBitSet,
+}
+
+impl BitUniverse {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
+}
+
+#[wasm_bindgen]
+impl BitUniverse {
+    pub fn new() -> BitUniverse {
+        let width = 64;
+        let height = 64;
+
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, i % 2 == 0 || i % 7 == 0);
+        }
+
+        BitUniverse { width, height, cells, }
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for column in 0..self.width {
+                let idx = self.get_index(row, column);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, column);
+
+                next.set(idx, match (cell, live_neighbors) {
+                    (true, x) if x < 2 => false,
+                    (true, 2) | (true, 3) => true,
+                    (true, x) if x > 3 => false,
+                    (false, 3) => true,
+                    (otherwise, _) => otherwise,
+                });
+            }
+        }
+
+        self.cells = next
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
     }
 }
--- wasm-app/src/counter.ts
+++ wasm-app/src/counter.ts
@@ -1,4 +1,5 @@
-import { Universe } from 'wasm-game-of-life'
+import { Universe, BitUniverse, Cell } from 'wasm-game-of-life'
+import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg.wasm'
 
 export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
@@ -11,8 +12,85 @@ export function setupCounter(element: HTMLButtonElement) {
 }
 
 export function setupWasmGreet(_: HTMLButtonElement) {
+  renderUniverseUsingCanvas2D()
+}
+
+export function renderUniverseUsingCanvas2D() {
+  const CELL_SIZE = 5
+  const GRID_COLOR = '#cccccc'
+  const DEAD_COLOR = '#ffffff'
+  const ALIVE_COLOR = '#000000'
+
+  const universe = BitUniverse.new()
+  const width = universe.width()
+  const height = universe.height()
+
+  const canvas = <HTMLCanvasElement>document.getElementById('game-of-life-canvas')
+  if (!canvas) return
+  canvas.height = (CELL_SIZE + 1) * height + 1
+  canvas.width = (CELL_SIZE + 1) * width + 1
+
+  const ctx = canvas.getContext('2d')
+  if (!ctx) return
+
+  const drawGrid = () => {
+    ctx.beginPath()
+    ctx.strokeStyle = GRID_COLOR
+
+    for (let i = 0; i <= width; i++) {
+      ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0)
+      ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1)
+    }
+
+    for (let j = 0; j <= height; j ++) {
+      ctx.moveTo(0, j * (CELL_SIZE + 1) + 1)
+      ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1)
+    }
+
+    ctx.stroke()
+  }
+
+  const bitIsSet = (n: number, arr: number[]) => {
+    const byte = Math.floor(n / 8)
+    const mask = 1 << (n % 8)
+    return (arr[byte] & mask) === mask
+  }
+  const getIndex = (row: number, column: number) => row * width + column
+  const drawCells = () => {
+    const cellsPtr = universe.cells()
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height)
+    
+    ctx.beginPath()
+
+    for (let row = 0; row < height; row++) {
+      for (let col = 0; col < width; col++) {
+        const idx = getIndex(row, col)
+
+        ctx.fillStyle = bitIsSet(idx, cells) ? DEAD_COLOR : ALIVE_COLOR
+        ctx.fillRect(
+          col * (CELL_SIZE + 1) + 1,
+          row * (CELL_SIZE + 1) + 1,
+          CELL_SIZE,
+          CELL_SIZE,
+        )
+      }
+    }
+  }
+
+  const renderLoop = () => {
+    universe.tick()
+
+    drawGrid()
+    drawCells()
+    requestAnimationFrame(renderLoop)
+  }
+
+  requestAnimationFrame(renderLoop)
+}
+
+export function renderUniverseUsingDOM() {
   const pre = document.querySelector('#game-of-life-pre')
-  const universe = Universe.new()
+  const universe = Universe.new_random()
 
   const renderLoop = () => {
     pre!.textContent = universe.render()

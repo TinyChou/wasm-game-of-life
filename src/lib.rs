296a66b448c4d4f164209ba2a5ddfbe5e884b506
TinyChou <zhoujinfu0124@163.com>
2023-03-21 21:42:17 +0800

    opt: 1) canvas `fillStyle` ops 2) reduce `live_neighbor_count` in rust mem allocator

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod utils;
 use wasm_bindgen::prelude::*;
 use std::fmt;
 use fixedbitset::FixedBitSet;
+use web_sys::console;
 
 // When the `wee_alloc` feature is enabled,
 // use `wee_alloc` as the global allocator.
@@ -242,18 +243,53 @@ impl BitUniverse {
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
         
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
-        }
+        let north = if row == 0 {
+            self.height - 1
+        } else {
+            row - 1
+        };
+    
+        let south = if row == self.height - 1 {
+            0
+        } else {
+            row + 1
+        };
+    
+        let west = if column == 0 {
+            self.width - 1
+        } else {
+            column - 1
+        };
+    
+        let east = if column == self.width - 1 {
+            0
+        } else {
+            column + 1
+        };
+    
+        let nw = self.get_index(north, west);
+        count += self.cells[nw] as u8;
+    
+        let n = self.get_index(north, column);
+        count += self.cells[n] as u8;
+    
+        let ne = self.get_index(north, east);
+        count += self.cells[ne] as u8;
+    
+        let w = self.get_index(row, west);
+        count += self.cells[w] as u8;
+    
+        let e = self.get_index(row, east);
+        count += self.cells[e] as u8;
+    
+        let sw = self.get_index(south, west);
+        count += self.cells[sw] as u8;
+    
+        let s = self.get_index(south, column);
+        count += self.cells[s] as u8;
+    
+        let se = self.get_index(south, east);
+        count += self.cells[se] as u8;
 
         count
     }
@@ -262,8 +298,8 @@ impl BitUniverse {
 #[wasm_bindgen]
 impl BitUniverse {
     pub fn new() -> BitUniverse {
-        let width = 64;
-        let height = 64;
+        let width = 64 * 2;
+        let height = 64 * 2;
 
         let size = (width * height) as usize;
         let mut cells = FixedBitSet::with_capacity(size);
@@ -276,34 +312,42 @@ impl BitUniverse {
     }
 
     pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
+        let _timer = Timer::new("BitUniverse::tick");
+        let mut next = {
+            let _timer = Timer::new("allocate next cells");
+            self.cells.clone()
+        };
 
-        for row in 0..self.height {
-            for column in 0..self.width {
-                let idx = self.get_index(row, column);
-                let cell = self.cells[idx];
-                let live_neighbors = self.live_neighbor_count(row, column);
+        {
+            let _timer = Timer::new("new generation");
+            for row in 0..self.height {
+                for column in 0..self.width {
+                    let idx = self.get_index(row, column);
+                    let cell = self.cells[idx];
+                    let live_neighbors = self.live_neighbor_count(row, column);
 
-                // log!(
-                //     "cell[{}, {}] is initially {:?} and has {} live neighbors",
-                //     row,
-                //     column,
-                //     cell,
-                //     live_neighbors,
-                // );
+                    // log!(
+                    //     "cell[{}, {}] is initially {:?} and has {} live neighbors",
+                    //     row,
+                    //     column,
+                    //     cell,
+                    //     live_neighbors,
+                    // );
 
-                next.set(idx, match (cell, live_neighbors) {
-                    (true, x) if x < 2 => false,
-                    (true, 2) | (true, 3) => true,
-                    (true, x) if x > 3 => false,
-                    (false, 3) => true,
-                    (otherwise, _) => otherwise,
-                });
+                    next.set(idx, match (cell, live_neighbors) {
+                        (true, x) if x < 2 => false,
+                        (true, 2) | (true, 3) => true,
+                        (true, x) if x > 3 => false,
+                        (false, 3) => true,
+                        (otherwise, _) => otherwise,
+                    });
 
-                // log!("    it becomes {:?}", self.cells[idx]);
+                    // log!("    it becomes {:?}", self.cells[idx]);
+                }
             }
         }
 
+        let _timer = Timer::new("free old cells");
         self.cells = next
     }
 
@@ -322,5 +366,22 @@ impl BitUniverse {
     pub fn toggle_cell(&mut self, row: u32, column: u32) {
         let idx = self.get_index(row, column);
         self.cells.toggle(idx);
+    }
+}
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
     }
 }
